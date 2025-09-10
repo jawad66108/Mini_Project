de7f914a8769ee613309c600571a2f2e4b09b1c2
jawad66108 <jawad66108@gmail.com>
2025-09-10 11:14:19 +0500

    Update codealpha.py

--- codealpha.py
+++ codealpha.py
@@ -1,18 +1,19 @@
-#==================mini project================
-# import random
-# ranNumber= random.randint(1,99)
-# b=0
-# while True:
-#     ChoiceOfUser=int(input("Guess the number[From 1 - 99]: "))
-#     b=b+1
-#     if(ChoiceOfUser==ranNumber):
-#         print ("Success: You Guessed it Correct!!!")
-#         break
-#     elif(b==6):
-#         print ("Try Again\nYou are out of moves!!!\nCorrect number was: ",ranNumber)
-#         break
-#     elif(ChoiceOfUser>ranNumber):
-#         print("----Try Again----\nYour Guess is larger than the number\nMoves left are: ",6-b)
-#     elif(ChoiceOfUser<ranNumber):
-#         print("----Try Again----\nYour Guess is smaller than the number\nMoves left are: ",6-b)
+==================mini project================
+import random
+ranNumber= random.randint(1,99)
+b=0
+while True:
+    ChoiceOfUser=int(input("Guess the number[From 1 - 99]: "))
+    b=b+1
+    if(ChoiceOfUser==ranNumber):
+        print ("Success: You Guessed it Correct!!!")
+        break
+    elif(b==6):
+        print ("Try Again\nYou are out of moves!!!\nCorrect number was: ",ranNumber)
+        break
+    elif(ChoiceOfUser>ranNumber):
+        print("----Try Again----\nYour Guess is larger than the number\nMoves left are: ",6-b)
+    elif(ChoiceOfUser<ranNumber):
+        print("----Try Again----\nYour Guess is smaller than the number\nMoves left are: ",6-b)
+
 
