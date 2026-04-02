e708aa11bdc1a3fb6a1424365416b0697fd7bfa0
Jawad Hussain <jawad66108@gmail.com>
2026-04-02 22:57:32 +0500

    Add files via upload

---
+++ D1.0.cpp
@@ -0,0 +1,38 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[5]= {23,4323,124,214,2};
+//     for(int i=4;i>=0;i--){
+//         cout<<arr[i]<<"\n";
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[5]={23,34,2,35,5};
+//     int max=0,min=arr[0];
+//     for(int i=0;i<5;i++){
+//         if(min>arr[i]){
+//             min=arr[i];
+//         }
+//         if(max<arr[i]){
+//             max=arr[i];
+//         }
+        
+//     }
+//     cout<<"Max: "<<max<<"\nMin: "<<min;
+// }
+
+// #include<iostream>
+// #include<algorithm>
+// int main(){
+//     int arr[5]={23,4,55,66,78};
+    
+//     int max = *max_element(arr, arr+5);
+//     int min = *min_element(arr, arr+5);
+    
+//     cout<<"Max: "<<max<<"\nMin: "<<min;
+
+// }
+
